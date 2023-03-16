70ca4aaeadec59a924b7eff245d339c05c38d44d
joanawuor00 <joanawuor00@gmail.com>
2023-03-16 12:34:37 -0700

    printing a line usig printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main-a program that prints a line of text using printf function
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+	Return(0);
+}
