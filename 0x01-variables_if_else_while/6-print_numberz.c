e941a87f854664685cbc48b8c676d741c81812c9
joanawuor00 <joanawuor00@gmail.com>
2023-03-19 09:01:56 -0700

    printing numbers using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - program to print 0 to 9 using the putchar function
+ *
+ * Return: 0 (Always)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
