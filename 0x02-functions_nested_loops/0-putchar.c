695b519595ecb8f4dab455dbbc5d4ee0ff98dee2
joanawuor00 <joanawuor00@gmail.com>
2023-03-22 05:05:15 -0700

    putchar alphabet

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - a program the print the alphabet putchar
+ *
+ * Return: 0 (Always)
+ */
+int main(void)
+{
+	_putvhar('_');
+	_putchar(p);
+	_putchar(u);
+	_putchar(t);
+	_putchar(c);
+	_putchar(h);
+	_putchar(a);
+	_putchar(r);
+	return (0);
+}
