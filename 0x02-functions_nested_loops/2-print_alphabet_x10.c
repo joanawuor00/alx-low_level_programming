15257fcc2769136d8871eb42b7700bc7fa380459
joanawuor00 <joanawuor00@gmail.com>
2023-03-22 05:55:50 -0700

    print alphabet 10times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,12 +2,12 @@
 /**
  * print_alphabet_x10 - prints alphabets 10times
  */
-void print_alphabet_x10(void);
+void print_alphabet_x10(void)
 {
 	int i;
 	char k;
 
-	for (i = '1'; i <= '10'; i++)
+	for (i = 1; i <= 10; i++)
 	{
 		for (k = 'a'; k <= 'z'; k++)
 		{
